6fb4523c635ba4682bc53c0a8c2d7745d6fb84e9
tushar230123 <123529235+tushar230123@users.noreply.github.com>
2023-02-07 13:09:07 +0530

    4 files added, 4 deleted

--- src/app/customer-list/customer-list.component.ts
+++ src/app/customer-list/customer-list.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 
-import { products } from '../products';
+import { customers } from '../customers';
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: 'app-customers-list',
+  templateUrl: './customers-list.component.html',
+  styleUrls: ['./customers-list.component.css'],
 })
-export class ProductListComponent {
-  products = products;
+export class CustomersListComponent {
+  customers = customers;
 
   share() {
     window.alert('The product has been shared!');
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
